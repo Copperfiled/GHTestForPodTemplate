b85663de544ccfbc58949ef0858e30848f46a363
Copperfiled <guan.dassy@gmail.com>
2016-09-07 11:17:55 +0800

    GHTestForPodTemplate Initial Commit

---
+++ Example/Pods/Target Support Files/GHTestForPodTemplate/GHTestForPodTemplate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GHTestForPodTemplate : NSObject
+@end
+@implementation PodsDummy_GHTestForPodTemplate
+@end
---
+++ Example/Pods/Target Support Files/Pods-GHTestForPodTemplate_Tests/Pods-GHTestForPodTemplate_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GHTestForPodTemplate_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GHTestForPodTemplate_TestsVersionString[];
+
